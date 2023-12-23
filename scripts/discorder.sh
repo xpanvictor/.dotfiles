0f34a2154eb48c47820c598b7e31369fef930fef
xpanvictor <xpanvictor@gmail.com>
2023-12-23 16:09:32 +0100

    feat(script): discord updater

---
+++ scripts/discorder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Variables
+download_dir="/home/xpan/Downloads/"
+download_file="discord_down_temp.tar.gz"
+
+# get the new package
+wget -O "${download_dir}${download_file}" 'https://discord.com/api/download/stable?platform=linux&format=tar.gz'
+
+echo "Downloaded into ${download_dir}${download_file}"
+sudo tar -xzvf "${download_dir}${download_file}" -C /opt
+
+# delete the tar download_file
+rm "${download_dir}${download_file}"
